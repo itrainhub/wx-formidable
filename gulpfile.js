1e6e121c21120f3841d519118aa0ba79e66420df
itrainhub <zhliro1102@gmail.com>
2021-04-16 10:07:47 +0800

    chore: 搭建项目结构

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp')
+var babel = require('gulp-babel')
+var uglify = require('gulp-uglify')
+var rename = require('gulp-rename')
+var del = require('del')
+ 
+var paths = {
+  scripts: {
+    src: 'lib/index.js',
+    dest: 'dist/'
+  }
+}
+
+const clean = () => del([ 'dist' ])
+
+function scripts() {
+  return gulp.src(paths.scripts.src, { sourcemaps: false })
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(rename(function(path) {
+      path.basename = "wx-formidable.min"
+    }))
+    .pipe(gulp.dest(paths.scripts.dest))
+}
+ 
+function watch() {
+  gulp.watch(paths.scripts.src, scripts)
+}
+
+var build = gulp.series(clean, gulp.parallel(scripts))
+
+exports.clean = clean
+exports.scripts = scripts
+exports.watch = watch
+exports.build = build
+
+exports.default = build
